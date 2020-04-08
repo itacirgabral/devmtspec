8d749da8d9b9cca8b925965db4058bebac154492
Itacir Gabral <itacirgabral@oniq.com.br>
2020-04-08 13:19:47 -0400

    soma 1 algarismo

--- src/somastr/somastr.spec.js
+++ src/somastr/somastr.spec.js
@@ -1,8 +1,14 @@
 var assert = require("assert");
 var soma = require("../somastr");
 
-describe("# DESCRIÇÃO DE UM BLOCO DE CASOS", function () {
-  it("## DESCRIÇÃO DE UM CASO", function () {
-    assert.equal('1', '2', "OPS MENSAGEM DE ERRO");
+describe("# FUNÇÃO SOMA STRING", function () {
+  it("## SOMA 1 ALGARISMO", function () {
+    var a = '3';
+    var b = '4';
+    var gabarito = '7';
+
+    var resultado = soma(a, b)
+
+    assert.equal(resultado, gabarito);
   });
 });
