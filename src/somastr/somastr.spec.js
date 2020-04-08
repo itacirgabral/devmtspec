0b12cc11b3c939874c4b9d95ad97966a42c7ed64
Itacir Gabral <itacirgabral@oniq.com.br>
2020-04-08 10:09:38 -0400

    bloco 3

---
+++ src/somastr/somastr.spec.js
@@ -0,0 +1,8 @@
+var assert = require("assert");
+var soma = require("../somastr");
+
+describe("# DESCRIÇÃO DE UM BLOCO DE CASOS", function () {
+  it("## DESCRIÇÃO DE UM CASO", function () {
+    assert.equal('1', '2', "OPS MENSAGEM DE ERRO");
+  });
+});
