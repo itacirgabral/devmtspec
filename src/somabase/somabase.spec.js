976399c7f6c8ca67a1132b37e3635ced6e17f5f9
Itacir Gabral <itacirgabral@oniq.com.br>
2020-04-09 14:28:41 -0400

    cross base

--- src/somabase/somabase.spec.js
+++ src/somabase/somabase.spec.js
@@ -3,13 +3,21 @@ var assert = require("assert");
 var base10q = ['Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 var base10n = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
+function soma (a, b) {
+  var ai = base10q.indexOf(a);
+  var bi = base10q.indexOf(b);
+  var abi = ai + bi;
+
+  return base10q[abi]
+}
+
 describe("# SOMA EM QUANQUER BASE", function () {
   it("## DESCRIÇÃO DE UM CASO", function () {
     var a = 'T';
     var b = 'U';
     var gabarito = 'X';
 
-    var resultado
+    var resultado = soma(a, b)
 
     assert.equal(resultado, gabarito);
   });
